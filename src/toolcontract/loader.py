c0e3f8c3adf0811d465a26431a75ddbe29c9dde1
wkdtjgus <seohyun050408@gmail.com>
2026-08-08 18:47:44 +0900

    bb

--- src/toolcontract/loader.py
+++ src/toolcontract/loader.py
@@ -1,9 +1,13 @@
-
 import json
 
 
 def load_tools(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
-        tools = json.load(file)
+        tool = json.load(file)
 
-    return tools
+    return tool
+
+
+if __name__ == "__main__":
+    tool = load_tools("examples/tool.json")
+    print(tool)
